e2b3dc1b466da3c1b2756ed13a5f7fd0ddb1a26e
angt <angt@users.noreply.github.com>
2016-01-17 10:56:55 +0100

    Use only v[0-9].* tags to get VERSION

--- version.sh
+++ version.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-[ -z "${VERSION}" ] && VERSION=`git describe --tags --always 2>/dev/null | \
-                                fgrep .` \
+[ -z "${VERSION}" ] && VERSION=`git describe --tags --match='v[0-9].*' 2>/dev/null` \
                     && VERSION=${VERSION#v}
 
 [ -z "${VERSION}" ] && VERSION=`cat VERSION 2>/dev/null`
