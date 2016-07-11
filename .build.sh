f4e94a9089c2996c3eabe7a335c71675e66273da
Adrien Gallouët <adrien@gallouet.fr>
2016-07-11 10:24:47 +0000

    Remove -flto for now

--- .build.sh
+++ .build.sh
@@ -4,10 +4,10 @@ export CC="gcc -static"
 
 git clone https://github.com/jedisct1/libsodium --depth=1 --branch stable
 cd libsodium || exit 1
-./autogen.sh && ./configure CFLAGS=-flto LDFLAGS=-flto --enable-minimal --disable-shared --prefix=/usr && make install
+./autogen.sh && ./configure --enable-minimal --disable-shared --prefix=/usr && make install
 cd ..
 
-./autogen.sh && ./configure CFLAGS=-flto LDFLAGS=-flto && make
+./autogen.sh && ./configure && make
 [ -x glorytun ] || exit 1
 
 mkdir -p deploy
