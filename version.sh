8ff9d3d7a1481bd82578d98798360cf219f3b73a
Adrien Gallouët <adrien@gallouet.fr>
2018-11-22 14:19:32 +0000

    Force version.sh to check only local .git
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- version.sh
+++ version.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-[ -z "${VERSION}" ] && VERSION="$(git describe --tags --match='v[0-9].*' 2>/dev/null)" \
-                    && VERSION="${VERSION#v}"
+export GIT_DIR=.git
+export GIT_WORK_TREE=.
 
-[ -z "${VERSION}" ] && VERSION="$(git rev-parse HEAD 2>/dev/null)"
+[ -z "$VERSION" ] && VERSION="$(git describe --tags --match='v[0-9].*' 2>/dev/null)" \
+                  && VERSION="${VERSION#v}"
 
-[ -z "${VERSION}" ] && VERSION="$(cat VERSION 2>/dev/null)"
+[ -z "$VERSION" ] && VERSION="$(git rev-parse HEAD 2>/dev/null)"
 
-[ -z "${VERSION}" ] && VERSION="0.0.0"
+[ -z "$VERSION" ] && VERSION="$(cat VERSION 2>/dev/null)"
 
-printf "%s" "${VERSION}" | tee VERSION
+[ -z "$VERSION" ] && VERSION="0.0.0"
+
+printf "%s" "$VERSION" | tee VERSION
