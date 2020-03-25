28a978f276b3602b8d4cb5a7fcdf28555154d409
Adrien Gallouët <adrien@gallouet.fr>
2020-03-25 07:32:34 +0000

    Fix the last fix :)
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- src/tun.c
+++ src/tun.c
@@ -131,14 +131,22 @@ tun_create_by_name(char *name, size_t len, const char *dev_name)
 static int
 tun_create_by_name(char *name, size_t len, const char *dev_name)
 {
-    int ret = snprintf(name, len, "/dev/%s", dev_name);
+    char tmp[64];
+    int ret = snprintf(name, len, "%s", dev_name);
 
     if (ret <= 0 || (size_t)ret >= len) {
         errno = EINVAL;
         return -1;
     }
 
-    return open(name, O_RDWR);
+    ret = snprintf(tmp, sizeof(tmp), "/dev/%s", dev_name);
+
+    if (ret <= 0 || (size_t)ret >= sizeof(tmp)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    return open(tmp, O_RDWR);
 }
 
 #endif /* not __APPLE__ */
