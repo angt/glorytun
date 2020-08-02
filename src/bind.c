ed304c1e1e1d41f39875437661aa4036b5ead9b4
Adrien Gallouët <adrien@gallouet.fr>
2020-08-02 14:27:30 +0000

    Update mud
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- src/bind.c
+++ src/bind.c
@@ -176,13 +176,6 @@ gt_bind(int argc, char **argv)
                persist ? "en" : "dis", tun_name);
     }
 
-    if (peer_addr.ss_family) {
-        if (mud_peer(mud, (struct sockaddr *)&peer_addr)) {
-            perror("mud_peer");
-            return 1;
-        }
-    }
-
     const int ctl_fd = ctl_create(tun_name);
 
     if (ctl_fd == -1) {
@@ -298,7 +291,9 @@ gt_bind(int argc, char **argv)
                 case CTL_NONE:
                     break;
                 case CTL_STATE:
-                    if (mud_set_state(mud, (struct sockaddr *)&req.path.addr,
+                    if (mud_set_state(mud,
+                                      (struct sockaddr *)&req.path.addr,
+                                      (struct sockaddr *)&peer_addr,
                                       req.path.state,
                                       req.path.rate_tx,
                                       req.path.rate_rx,
